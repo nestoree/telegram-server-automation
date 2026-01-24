c977d6aa55e89b6b4cd5edd5f7005b9828ba3344
nestore__ <termuxchannelyt@gmail.com>
2026-01-24 00:49:10 +0000

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/nestore/telegram_env/bin/activate
-exec python /home/nestore/scripts/telegram_bot.py
+source /home/YOUR_USER/telegram_env/bin/activate # Change this for your user and your env
+exec python /home/YOUR_USER/scripts/telegram_bot.py # Change this for your user
